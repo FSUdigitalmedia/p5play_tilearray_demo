2d8dc5997a017cfc1945278341e81045d8adf912
Rob Duarte <me@robduarte.com>
2023-10-31 09:46:11 -0400

    how it looked at the end of the demo

--- sketch.js
+++ sketch.js
@@ -1,58 +1,65 @@
-
-let maze = [
-    '@@@@@@@@@',
-    '@   @   @',
-    '@@@ @ @ @',
-    '@@@   @ @',
-    '@@@@@   @',
-    '@@@@@@@@@'
-];
 let avatar;
 let tileImg, avatarImg;
+let map = 
+    ['@@@@@@@@@',
+     '@   @   @',
+     '@@@ @ @ @',
+     '@@@   @ @',
+     '@@@@@   @',
+     '@@@@@@@@@'
+    ]
     
 function preload() {
-    // load images here
     avatarImg = loadImage('assets/sprite.png');
     tileImg = loadImage('assets/tile.png');
 }
 
 function setup() {
-    let canvas = new Canvas(300,450, 'pixelated x2');
+    new Canvas(300,450, 'pixelated x2');
     allSprites.pixelPerfect = true;
+    allSprites.rotationLock = true;
     allSprites.tileSize = 32;
 
-    avatar = new Sprite(1,1,1,1);
+    avatar = new Sprite(1.5,1.5, 1,1);
     avatar.img = avatarImg;
-    avatar.layer = 1;
 
     let brick1 = new Group();
     brick1.img = tileImg;
     brick1.collider = 'none';
     brick1.tile = '@';
 
-    new Tiles(maze, 0, 0, 1, 1);
+    new Tiles(map,.5, .5, 1, 1);
 }
 
 function draw() {
     clear();
 
-    if (kb.pressed('up') && canMove(avatar.x, avatar.y-1)) {
+    if (kb.pressed('up') && isOpen(avatar.x,avatar.y-1)) {
         avatar.y--;
     }
-    else if (kb.pressed('left') && canMove(avatar.x-1, avatar.y)) {
+    else if (kb.pressed('left') && isOpen(avatar.x-1,avatar.y)) {
         avatar.x--;
     }
-    else if (kb.pressed('right') && canMove(avatar.x+1, avatar.y)) {
-        avatar.x++;
+    else if (kb.pressed('right') && isOpen(avatar.x+1,avatar.y)) {
+        avatar.moveTo( createVector(avatar.x+1,avatar.y), .1 )
+        //avatar.x++;
     }
-    else if (kb.pressed('down') && canMove(avatar.x, avatar.y+1)) {
+    else if (kb.pressed('down') && isOpen(avatar.x,avatar.y+1)) {
         avatar.y++;
     }
 }
 
-function canMove(x, y) {
+
+function isOpen(x, y) {
     let i = floor(x);
     let j = floor(y);
-    let tile = maze[j][i];
-    return tile == ' ';
+    let tile = map[j][i];
+    if (tile == ' ') {
+        return true;
+    } else {
+        console.log('blocked!');
+        return false;
+    }
+    //return tile == ' ';
+
 }
